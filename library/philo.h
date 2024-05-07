27d9cea756e94eca0e8c69c337118594590362c8
ileyassu <ilyas.benaissa@um5r.ac.ma>
2024-05-07 12:01:20 +0100

    New Updates

--- library/philo.h
+++ library/philo.h
@@ -7,21 +7,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct s_utils
-{
-    int philo_num;
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int num_of_time_to_eat;
-}   t_utils;
-
 typedef struct s_philo
 {
     pthread_t thread_id;
     pthread_mutex_t lock;
     int id;
 }   t_philo;
+
+typedef struct s_utils
+{
+    t_philo *philo;
+    int philo_num;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int num_of_time_to_eat;
+}   t_utils;
 
 int	ft_isdigit(int arg);
 int	ft_atoi(const char *pt);
--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main(int ac, char **av)
     (void)ac;
     t_utils utils;
     parse(&utils, ac, av);
-    t_philo philo[ft_atoi(av[1])];
+    t_philo philo[ft_atoi(av[1])]; //creating a tab of philosophers. (should adjust it for the norms)
     philo_init(&utils,philo ,av);
+    utils.philo = philo;
 }
